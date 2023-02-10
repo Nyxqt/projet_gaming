3b7608a27d5755372edc113d4e28cddb0a371762
Nyxqt <geiss.gamer@gmail.com>
2023-02-10 12:30:48 +0100

    header progress

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,19 +9,33 @@
 </head>
 <body>
     <header>
-        <nav class="navbar p-0">
-            <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg p-0">
+            <div class="container-fluid d-flex justify-content-between p-0" data-bs-theme="dark">
                 <a class="navbar-brand brand_title d-flex flex-column text-center ms-2" href="#">
                     <span class="title_top">Nyx</span> 
                     <span class="title_bot mt-n1">gaming</span>
                 </a>
-
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link title_ff hover_yellow fs-2 active" aria-current="page" href="#">Stratégie</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link title_ff hover_yellow fs-2" href="#">Action</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link title_ff hover_yellow fs-2" href="#">Indé</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link title_ff hover_yellow fs-2" href="#">Contact</a>
+                    </li>
+                </ul>
+                <div class="navbar-nav me-5 d-flex">
+                    <a href="#"><i class="fa-solid fa-cart-shopping fa-2xl me-3 hover_yellow"></i></a>
+                    <a href="#"><i class="fa-solid fa-circle-user fa-2xl ms-2 hover_yellow"></i></a>
+                </div>
             </div>
         </nav>
     </header>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<?php require('../assets/vendors/externalsJS.php') ?>
 </body>
 </html>
