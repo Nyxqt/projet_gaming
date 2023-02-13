20ff57411eae631dc00b1df091408baf78063e38
Nyxqt <geiss.gamer@gmail.com>
2023-02-13 16:26:57 +0100

    homepage progress

--- templates/homepage.php
+++ templates/homepage.php
@@ -17,7 +17,10 @@
                 </a>
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                    <a class="nav-link title_ff hover_yellow fs-2 active" aria-current="page" href="#">Stratégie</a>
+                    <a class="nav-link title_ff hover_yellow fs-2 current_page" href="#">Stratégie</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link title_ff hover_yellow fs-2" href="#">TG Matsu</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link title_ff hover_yellow fs-2" href="#">Action</a>
@@ -28,6 +31,9 @@
                     <li class="nav-item">
                     <a class="nav-link title_ff hover_yellow fs-2" href="#">Contact</a>
                     </li>
+                    <li class="nav-item">
+                    <a class="nav-link title_ff hover_yellow fs-2" href="#"><i class="fa-solid fa-magnifying-glass fa-xs"></i></a>
+                    </li>
                 </ul>
                 <div class="navbar-nav me-5 d-flex">
                     <a href="#"><i class="fa-solid fa-cart-shopping fa-2xl me-3 hover_yellow"></i></a>
@@ -36,6 +42,23 @@
             </div>
         </nav>
     </header>
+    <main>
+        <div class="main_highlight container-fluid p-0">
+            <picture>
+                <img src="../assets/images/bg_legacy_hogwarts.jpg" alt="Hogwarts Legacy">
+                <div class="container d-flex flex-column p-0">
+                    <p class="title_ff fs-2 text-white">Hogwarts Legacy : L'Héritage de Poudlard<br />
+                        <span class="price yellow fs-1 text_ff">25</span>
+                    </p>
+                </div>
+            </picture>
+        </div>
+        <div class="main_tendency p-0">
+            <div class="container p-0">
+                <h1 class="mt-3 text-white title_ff fs-2">Tendances</h1>
+            </div>
+        </div>
+    </main>
 <?php require('../assets/vendors/externalsJS.php') ?>
 </body>
 </html>
